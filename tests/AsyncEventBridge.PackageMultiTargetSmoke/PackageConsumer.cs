7d5312f5eb7e0a9a93319e2842d2e1669db0a6e0
Perry <its_Q@hotmail.com>
2026-09-22 11:35:00 +0200

    Exercise generated APIs in multi-target package consumer

---
+++ tests/AsyncEventBridge.PackageMultiTargetSmoke/PackageConsumer.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AsyncEventBridge;
+
+[assembly: GenerateAsyncEventsFor(typeof(System.Timers.Timer))]
+
+namespace AsyncEventBridge.PackageMultiTargetSmoke
+{
+    [GenerateAsyncEvents]
+    public sealed class Sensor
+    {
+        public event EventHandler<SensorEventArgs>? ValueChanged;
+
+        public void RaiseValue(int value)
+        {
+            ValueChanged?.Invoke(this, new SensorEventArgs(value));
+        }
+    }
+
+    public sealed class SensorEventArgs : EventArgs
+    {
+        public SensorEventArgs(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; }
+    }
+
+    public static class PackageConsumer
+    {
+        public static Task<SensorEventArgs> WaitForValueAsync(
+            Sensor sensor,
+            CancellationToken cancellationToken = default)
+        {
+            return sensor.ValueChangedAsync(cancellationToken);
+        }
+
+        public static IAsyncEnumerable<SensorEventArgs> ReadValues(
+            Sensor sensor,
+            CancellationToken cancellationToken = default)
+        {
+            return sensor.ValueChangedStream(cancellationToken);
+        }
+
+        public static Task<System.Timers.ElapsedEventArgs> WaitForElapsedAsync(
+            System.Timers.Timer timer,
+            CancellationToken cancellationToken = default)
+        {
+            return timer.ElapsedAsync(cancellationToken);
+        }
+    }
+}
